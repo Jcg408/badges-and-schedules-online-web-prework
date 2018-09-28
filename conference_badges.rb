e699511c4bd779496ebce55789f75038f6afab18
Jessica Gladstone <jcg321@comcast.net>
2018-09-28 17:42:59 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ end
 def batch_badge_creator (attendees)
   list = []
   attendees.each do |badges|
-    list.push("Hello, my name is #{badges}")
+    list.push("Hello, my name is #{badges}.")
     
  end
  end
